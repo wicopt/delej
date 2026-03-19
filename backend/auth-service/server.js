5a0c6e39f5607703654146b4158d52b605561b0c
Maria Ushakova <mary@DESKTOP-NASUVRS.localdomain>
2026-03-19 17:12:45 +0700

    db init

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -4,8 +4,8 @@ const registrationRoutes = require("./routes/registration")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("hi there!")
